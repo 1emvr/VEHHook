aaa569a463783cff30bd15d4eff358b8d0d49dfc
1emvr <lemur.proxymslns@outlook.com>
2024-10-28 03:02:57 -0400

    fixing minor issues

--- hook.cpp
+++ hook.cpp
@@ -18,7 +18,7 @@ LONG WINAPI ExceptionHandler(_EXCEPTION_POINTERS *exception) {
         for (fn_index = g_hooks.begin(); fn_index != g_hooks.end(); ++fn_index) {
             if (exception->ContextRecord->Rip == (uintptr_t) fn_index->first) {
 
-                WriteProcessMemory((HANDLE)(ULONG_PTR) -1, fn_index->first, &fn_index->second, 1, NULL);
+                WriteProcessMemory((HANDLE)(ULONG_PTR) -1, fn_index->first, &fn_index->second, 1, nullptr);
                 exception->ContextRecord->EFlags |= 0x100;
 
                 // TODO: dispatch analysis by type??
@@ -37,7 +37,7 @@ LONG WINAPI ExceptionHandler(_EXCEPTION_POINTERS *exception) {
             if ((exception->ContextRecord->Rip - (uintptr_t) fn_index->first) < 8) {
 
                 uint8_t bp_opcode = 0xCC;
-                WriteProcessMemory((HANDLE)(ULONG_PTR) -1, fn_index->first, &bp_opcode, 1, NULL);
+                WriteProcessMemory((HANDLE)(ULONG_PTR) -1, fn_index->first, &bp_opcode, 1, nullptr);
 
                 exception->ContextRecord->EFlags &= ~0x100;
                 retval = EXCEPTION_CONTINUE_EXECUTION;
@@ -60,26 +60,26 @@ BOOL HookFunction(const char *module_name, const char *func_name) {
     
 
     // only hook if veh already set
-    if (g_is_veh_set == false) {
-        LOG_ERROR("hook_function : cannot hook while g_is_veh_set is false");
+    if (g_veh_is_set == false) {
+        // LOG_ERROR("hook_function : cannot hook while g_is_veh_set is false");
         goto defer;
     }
         
     HMODULE module_base = GetModuleHandleA(module_name);
     if (!module_base) {
-        LOG_ERROR("hook_function : GetModuleHandleA failed to resolve %s", module_name);
+        // LOG_ERROR("hook_function : GetModuleHandleA failed to resolve %s", module_name);
         goto defer;
     }
 
     FARPROC func_base = GetProcAddress(module_base, func_name);
     if (!func_base) {
-        LOG_ERROR("hook_function : GetProcAddress failed to resolve %s!%s", module_name, func_name);
+        // LOG_ERROR("hook_function : GetProcAddress failed to resolve %s!%s", module_name, func_name);
         goto defer;
     }
 
-    // TODO: save byte and hooked function in a hashmap to unhook later
-    g_hooks.insert( {func_base, func_base[0]} );
-    bool success = WriteProcessMemory((HANDLE)(ULONG_PTR)-1, func_base, &bp_opcode, 1, nullptr);
+    // save byte and hooked function in g_hooks
+    g_hooks.insert( {func_base, ((uint8_t*) func_base)[0]} );
+    success = WriteProcessMemory((HANDLE)(ULONG_PTR)-1, func_base, &bp_opcode, 1, nullptr);
 
  defer:
     return success;
